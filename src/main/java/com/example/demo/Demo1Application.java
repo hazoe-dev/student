a1eb27a2537084d570c0f652fb3582b6d002dd24
hazoe-dev <hatran.dev@gmail.com>
2024-09-04 08:14:00 +0700

    init project

---
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import com.example.demo.entity.Student;
+import com.example.demo.repo.StudentRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Demo1Application implements CommandLineRunner {
+
+
+    @Autowired
+    StudentRepository repository;
+
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext appContext = SpringApplication.run(Demo1Application.class, args);
+        SearchServiceImpl searchService = appContext.getBean(SearchServiceImpl.class);
+        int result = searchService.search(new int[]{12, 5, 7}, 5);
+        System.out.println(result);
+
+    }
+
+    @Override
+    public void run(String... args) {
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+        logger.info("User id 10001 -> {}", repository.findById(100l));
+
+        logger.info("Inserting -> {}",
+                repository.save(new Student("Tara", "Berlin", "West", "1")));
+
+        logger.info("Update 2 -> {}",
+                repository.save(new Student(200l, "Zoe", "Berlin", "West", "1")));
+
+        repository.deleteById(300l);
+
+        logger.info("All users -> {}", repository.findAll());
+    }
+}
